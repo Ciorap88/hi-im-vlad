d35d6d482588c344e055dc92432f10ddb196be2a
Ciorap88 <vlad.014916@gmail.com>
2020-04-13 13:42:29 +0300

    Updates

---
+++ precache-manifest.cb517e751e644aa89e1e4315d9b6923b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95239cdf97cb484e5abfde4a07373dd3",
+    "url": "/hi-im-vlad/index.html"
+  },
+  {
+    "revision": "53c5850f2984a5083aeb",
+    "url": "/hi-im-vlad/static/css/main.2fc1ef41.chunk.css"
+  },
+  {
+    "revision": "eb5ca199d774e820ff54",
+    "url": "/hi-im-vlad/static/js/2.1298893f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hi-im-vlad/static/js/2.1298893f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53c5850f2984a5083aeb",
+    "url": "/hi-im-vlad/static/js/main.39ff5e0a.chunk.js"
+  },
+  {
+    "revision": "db17ad61cb6a84e91be7",
+    "url": "/hi-im-vlad/static/js/runtime-main.01eeef9f.js"
+  }
+]);
